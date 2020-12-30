e467207e173a3e9a23025fa2ba22d1050012aaf5
Nemer Y Tamimi <nemertamimi1999@icloud.com>
2020-12-30 03:18:59 +0200

    Update dbConf.php

--- Shared/dbConf.php
+++ Shared/dbConf.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbhost = "localhost";
-$dbuser = "c159_master";
-$dbpass = "c159_comp334";
-$dbname = "c159_project";
+$dbuser = "master";
+$dbpass = "comp334";
+$dbname = "dbschema_1170025";
 
 // create PDO Object:
 $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
